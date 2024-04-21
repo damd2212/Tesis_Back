dc8b1eee604a0125bc753f7bf955f854714382f3
damd2212 <53495267+damd2212@users.noreply.github.com>
2024-04-21 01:35:07 -0500

    prueba ruta

--- build.sh
+++ build.sh
@@ -8,3 +8,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Aquí definimos la ruta que queremos revisar
+ruta="/opt/render/project/src/.venv/bin/gunicorn"
+
+# Mostrar el contenido de la ruta
+echo "Contenido de la ruta $ruta:"
+ls -l $ruta
