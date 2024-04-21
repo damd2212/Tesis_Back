cac52be9c51f130ceda04c125937416d8cdc9258
damd2212 <53495267+damd2212@users.noreply.github.com>
2024-04-21 02:37:19 -0500

    No se

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 
 pip install -r requirements.txt
-
-sed -i 's/if __name__ == '\''__main__'\'':/if __name__ == '\''__main__'\'':\n    from opt.render.project.src.TesisApp.columnExtractor import ColumnExtractor/' /opt/render/project/src/.venv/bin/gunicorn
+ls -l "/opt/render/project/src"
+sed -i 's/if __name__ == '\''__main__'\'':/if __name__ == '\''__main__'\'':\n    from src.TesisApp.columnExtractor import ColumnExtractor/' /opt/render/project/src/.venv/bin/gunicorn
 cat /opt/render/project/src/.venv/bin/gunicorn
 
 python manage.py collectstatic --no-input
