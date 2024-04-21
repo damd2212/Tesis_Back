663eeab52688d9b61c8621d9fc64b52eca72c47f
damd2212 <53495267+damd2212@users.noreply.github.com>
2024-04-21 01:41:40 -0500

    s

--- build.sh
+++ build.sh
@@ -10,8 +10,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Aquí definimos la ruta que queremos revisar
-ruta="/opt/render/project/src/.venv/bin/gunicorn"
-
-# Mostrar el contenido de la ruta
-echo "Contenido de la ruta $ruta:"
-ls -l $ruta
+cat /opt/render/project/src/.venv/bin/gunicorn
