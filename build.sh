ce2f67243faaa72869c55f35f80833e91a828f2a
damd2212 <53495267+damd2212@users.noreply.github.com>
2024-04-21 02:09:16 -0500

    s

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ set -o errexit
 
 pip install -r requirements.txt
 
-ruta="/opt/render/project/src/TesisApp"
-ls -l $ruta
+# Archivo original
+archivo_original="/opt/render/project/src/.venv/bin/gunicorn"
 
-cat /opt/render/project/src/TesisApp/columnExtractor.py
-# Aquí definimos la ruta que queremos revisar
-echo "Contenido _________________"
-cat /opt/render/project/src/.venv/bin/gunicorn
+# Patrón de búsqueda y reemplazo
+patron_busqueda="from gunicorn.app.wsgiapp import run"
+patron_reemplazo="from TesisApp.columnExtractor import ColumnExtractor\n\n\0"
+
+# Editar el archivo usando sed
+sed -i "s/$patron_busqueda/$patron_reemplazo/g" "$archivo_original"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
