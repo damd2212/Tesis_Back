21d6af6249edc9c65ccb930e20ef893cd370652e
damd2212 <53495267+damd2212@users.noreply.github.com>
2024-04-21 23:14:17 -0500

    prueba en manage

--- TesisAPI/wsgi.py
+++ TesisAPI/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from TesisApp.columnExtractor import ColumnExtractor
 
-print("Directorio actual:", os.getcwd())
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TesisAPI.settings')
 
 application = get_wsgi_application()
--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ set -o errexit
 
 
 pip install -r requirements.txt
-#ls -l "/opt/render/project/src"
-#sed -i 's/if __name__ == '\''__main__'\'':/if __name__ == '\''__main__'\'':\n    from src.TesisApp.columnExtractor import ColumnExtractor/' /opt/render/project/src/.venv/bin/gunicorn
-#cat /opt/render/project/src/.venv/bin/gunicorn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
